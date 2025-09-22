dbf5c14c2677a99b9869354c678eb3c5d5a5be50
MustafaMemon0099 <muhammadfaisalmemon0099@gmail.com>
2025-09-22 21:26:39 +0500

    Added operators.js

---
+++ operators.js
@@ -0,0 +1,17 @@
+ let a = 10
+ let b = 20
+ let c = 30
+console.log( "a + b = " , c );
+console.log("a - b = ", a - b );
+console.log("a * b = " ,a * b);
+console.log(" a * b =" , a * b)
+console.log(" a % b =", a%b )
+console.log("a ** b =", a**b)
+console.log( "++a = ", ++a)
+console.log(" --a =", --a)
+ let x = 8
+ let y = 5
+ console.log("x =" , x, "& y = " ,y);
+a **= 4;
+console.log("x =", x )
+
