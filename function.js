755365617bd4cac7df79760759c254bef529f581
MustafaMemon0099 <muhammadfaisalmemon0099@gmail.com>
2025-10-01 23:14:24 +0500

    added function.js

---
+++ function.js
@@ -0,0 +1,30 @@
+// Normal function
+function myFunction() {
+  console.log("Welcome!");
+  console.log("Apna College");
+}
+myFunction();
+
+// Greet function
+function greet(name) {
+  console.log("Hello " + name);
+}
+greet("Mustafa");
+
+// Add function
+function add(a, b) {
+  return a + b;
+}
+let result = add(5, 7);
+console.log(result);
+
+// Arrow function
+let add2 = (a, b) => a + b;
+let answer = add2(5, 4);
+console.log(answer);
+
+// Intro function
+function myIntro() {
+  console.log("My name is Mustafa");
+}
+myIntro();
